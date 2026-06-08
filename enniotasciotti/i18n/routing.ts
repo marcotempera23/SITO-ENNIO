d658ed3a44b1e0e3d853ce49bcdcfe146c9cf6f6
Marco Tempera <marcotemperino8@gmail.com>
2026-06-09 00:19:04 +0200

    fix: set localePrefix to always to resolve 404 error

--- enniotasciotti/i18n/routing.ts
+++ enniotasciotti/i18n/routing.ts
@@ -3,5 +3,5 @@ import { defineRouting } from 'next-intl/routing';
 export const routing = defineRouting({
   locales: ['it', 'en'],
   defaultLocale: 'it',
-  localePrefix: 'as-needed',
+  localePrefix: 'always',
 });
